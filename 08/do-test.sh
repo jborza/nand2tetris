788ad1d6ebe7a8fce43450b6fd01cce2f5cdeded
Juraj Borza <jborza@gmail.com>
2020-08-02 14:06:03 +0200

    08 test script - added diff

--- 08/do-test.sh
+++ 08/do-test.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+# Usage: ./do-test TestDirectory/TestName
+# e.g. do-test ProgramFlow/BasicLoop
 
 DIR=$1
 PROG=$(echo $1 | cut -d / -f 2)
-#echo $DIR
-#echo $PROG
 
 python3 VMTranslator.py $DIR/$PROG.vm > $DIR/$PROG.asm
 python3 ../06/asm.py $DIR/$PROG.asm > $DIR/$PROG.hack
 ../../tools/CPUEmulator.sh $DIR/$PROG.tst
+diff $DIR/$PROG.cmp $DIR/$PROG.out
