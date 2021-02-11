0b4a4d930ead0cffbf06a24abee8dc59c125b178
jborza <jborza@gmail.com>
2021-02-11 22:06:47 +0100

    compilationengine with class declaration, classVarDec, type, subroutineDec

--- 10/CompilationEngine.py
+++ 10/CompilationEngine.py
@@ -23,7 +23,6 @@ def xmlprint(el, value):
 
 class CompilationEngine:
 
-
     def __init__(self, filename):
         self.tokenizer = JackTokenizer(filename)
 
@@ -34,51 +33,183 @@ class CompilationEngine:
     def xml_print_el(self):
         xmlprint(self.tokenizer.token_type, self.tokenizer.token)
 
+    def advanceSymbol(self, symbol):
+        if self.tokenizer.hasMoreTokens():
+            self.tokenizer.advance()
+        else:
+            raise SyntaxError('Symbol expected:' + symbol+ ', found end of stream')
+        if self.tokenizer.symbol() != symbol:
+            raise SyntaxError('Symbol expected:' + symbol)
+
+    def advanceKeyword(self, keyword):
+        if self.tokenizer.hasMoreTokens():
+           self.tokenizer.advance()
+        else:
+            raise SyntaxError('Keyword expected:' + keyword+ ', found end of stream')
+        if self.tokenizer.keyword() != keyword:
+            raise SyntaxError('Keyword expected:' + keyword)
+
+    def advanceTokenType(self, tokenType):
+        if self.tokenizer.hasMoreTokens():
+           self.tokenizer.advance()
+        else:
+            raise SyntaxError('Identifier expected, found end of stream')
+        if self.tokenizer.token_type != 'identifier':
+            raise SyntaxError('Identifier expected')
+
+    def advanceKeywords(self, *args):
+        if self.tokenizer.hasMoreTokens():
+           self.tokenizer.advance()
+        else:
+            raise SyntaxError('Keywords expected:' + args+ ', found end of stream')
+        if self.tokenizer.keyword() != keyword:
+            raise SyntaxError('Keywords expected:' + args)
+
+    def advanceAndGetType(self):
+        if self.tokenizer.hasMoreTokens():
+           self.tokenizer.advance()
+        else:
+            raise SyntaxError('type expected, found end of stream')
+        if self.is_type():
+            return self.tokenizer.token
+        else:
+            raise SyntaxError('type expected')
+
+    def is_type(self):
+        return self.tokenizer.keyword() in ['int', 'char', 'boolean'] or self.tokenizer.token_type == 'identifier'
+
+    def advanceAndGetReturnType(self):
+        self.advance()
+        if self.is_type() or self.tokenizer.keyword() == 'void':
+            return self.tokenizer.token
+        else:
+            raise SyntaxError('type expected')
+
+    def advanceToClassName(self):
+        self.advanceTokenType('identifier')
+        return self.tokenizer.identifier()
+
+    def advanceToVarName(self):
+        self.advanceTokenType('identifier')
+        return self.tokenizer.identifier()
+
+    def advanceToSubroutineName(self):
+        self.advanceTokenType('identifier')
+        return self.tokenizer.identifier()
+
+    def hasClassVarDec(self):
+        pass
+
+    def advance(self):
+        if self.tokenizer.hasMoreTokens():
+            self.tokenizer.advance()
+        else:
+            raise SyntaxError('found end of stream!')
+
     def compileClass(self):
         # 'class' className '{' classVarDec* subroutineDec* '}'
-        # first we expect keyword class
-        if self.tokenizer.hasMoreTokens():
-            self.tokenizer.advance()
-        if self.tokenizer.keyword() != 'class':
-            raise('class expected')
         print('<class>')
+        self.advanceKeyword('class')
         self.xml_print_el()
         # classname
-        if self.tokenizer.hasMoreTokens():
-            self.tokenizer.advance()
-        if self.tokenizer.token_type != 'identifier':
-            raise('identifier expected')
+        self.advanceToClassName()
+        className = self.tokenizer.identifier()
         self.xml_print_el()
-        # open bracket
-        if self.tokenizer.hasMoreTokens():
-            self.tokenizer.advance()
-        if self.tokenizer.symbol() != '{':
-            raise('{ expected after class opening')
+        # {
+        self.advanceSymbol('{')
         self.xml_print_el()
-        # classVarDec*
 
-        if self.tokenizer.hasMoreTokens():
-            self.tokenizer.advance()
+        self.advance()
+        # classVarDec*
+        while(self.tokenizer.keyword() in ['static', 'field']):
+            self.compileClassVarDec()
+           
         # subroutineDec*
         while (self.tokenizer.keyword() in ['constructor', 'function', 'method']):
             self.compileSubroutine()
-        pass
+           
+        # }
+        self.advanceSymbol('}')
+        self.xml_print_el()
+        print('</class>')
 
-    def compileClassVarDec(self):
+    def compileClassVarDec(self):    
         # ('static'|'field') type varName (',' varName)* ';'
-        pass
+        print('<classVarDec>')
+        # ('static'|'field')
+        self.xml_print_el()
+        # type
+        type = self.advanceAndGetType()
+        self.xml_print_el()
+        # varName
+        varName = self.advanceToVarName()
+        self.xml_print_el()
+        # ;
+        self.advanceSymbol(';')
+        self.xml_print_el()
+        print('</classVarDec>')
+        self.advance()
 
     def compileSubroutine(self):
-        print('subroutine')
+        print('<subroutineDec>')
+        kind = self.tokenizer.keyword()
+        self.xml_print_el()
+
+        # ( 'void' | type )
+        
+        return_type = self.advanceAndGetReturnType()
+        self.xml_print_el()
+
+        # subroutineName
+        name = self.advanceToSubroutineName()
+        self.xml_print_el()
+
+        # (
+        self.advanceSymbol('(')
+        self.xml_print_el()
+
+        # TODO parameterList
+        self.compileParameterList()
+
+        # (
+        self.advanceSymbol(')')
+        self.xml_print_el()
+
+        # subroutineBody
+        self.compileSubroutineBody()
+
+        print('</subroutineDec>')
+        self.advance()
         pass
 
-    def compileParameterList():
+    def compileSubroutineBody(self):
+        print('<subroutineBody>')
+        # {
+        self.advanceSymbol('{')
+        self.xml_print_el()
+
+        # varDec*
+        #TODO a structure to represent the *
+        self.varDec()
+
+        # statementes
+        self.compileStatements()
+
+        # }
+        self.advanceSymbol('}')
+        self.xml_print_el()
+        print('</subroutineBody>')
+
+
+    def compileParameterList(self):
+        print('<parameterList>')
+
+        print('</parameterList>')
+
+    def compileVarDec(self):
         pass
 
-    def compileVarDec():
-        pass
-
-    def compileStatements():
+    def compileStatements(self):
         pass
 
     def compileDo():
@@ -108,5 +239,7 @@ class CompilationEngine:
     def compileExpressionList():
         pass
 
-c = CompilationEngine('10/ArrayTest/Main.jack')
+#c = CompilationEngine('10/ArrayTest/Main.jack')
+c = CompilationEngine('10/ExpressionLessSquare/Main.jack')
+
 c.compileClass()
